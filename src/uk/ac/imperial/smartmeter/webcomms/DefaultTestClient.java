ca997f3411bb52cb2e3e31ce2936a89242a59829
bwindo <bw1313@ic.ac.uk>
2015-08-19 17:42:11 +0100

    Attempting to identify bug in RMI conns.5

--- src/uk/ac/imperial/smartmeter/webcomms/DefaultTestClient.java
+++ src/uk/ac/imperial/smartmeter/webcomms/DefaultTestClient.java
@@ -1,7 +1,7 @@
 package uk.ac.imperial.smartmeter.webcomms;
 
 public class DefaultTestClient {
-	public static Boolean localOverride = true;
+	public static Boolean localOverride = false;
 	public static String ipAddr = !localOverride?"155.198.117.240":LocalTestClient.ipAddr;
 	public static int HLCPort = !localOverride?6666:LocalTestClient.HLCPort;
 	public static int EDCPort = !localOverride?7777:LocalTestClient.EDCPort;
